85cfe734b696cc9f004f3025b6e96028ad48384f
Mathieu Benoit <mathben@technolibre.ca>
2024-04-02 18:18:49 -0400

    [ADD] using-opencv-tesseract script docker_install.sh

---
+++ using-opencv-tesseract/docker_install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+host=""
+password=""
+if [[ -z $host ]]; then
+    read -p "Enter your ip/host : " host
+fi
+
+if [[ -z $password ]]; then
+    read -s -p "Enter the password of root user : " password
+    echo
+fi
+
+docker run -it exemple_using_opencv_tesseract_1_0 /opt/axis/acapsdk/sysroots/x86_64-pokysdk-linux/usr/bin/eap-install.sh $host $password install
