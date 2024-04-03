8149b8b9a82c3d477aa7163712ac4f5c20315f8a
Mathieu Benoit <mathben@technolibre.ca>
2024-04-03 14:18:41 -0400

    [UPD] using-opencv-tesseract: docker install use sshpass for more automation

--- using-opencv-tesseract/docker_install.sh
+++ using-opencv-tesseract/docker_install.sh
@@ -14,7 +14,8 @@ docker run -it exemple_using_opencv_tesseract_1_0 /opt/axis/acapsdk/sysroots/x86
 
 # Bug replace lib
 # libstdc++.so.6.0.30
-scp lib/*.so* root@${host}:/mnt/flash/usr/local/packages/opencv_tesseract_app/lib/
+#scp lib/*.so* root@${host}:/mnt/flash/usr/local/packages/opencv_tesseract_app/lib/
+sshpass -p $password scp lib/*.so* root@${host}:/mnt/flash/usr/local/packages/opencv_tesseract_app/lib/
 
 docker run -it exemple_using_opencv_tesseract_1_0 /opt/axis/acapsdk/sysroots/x86_64-pokysdk-linux/usr/bin/eap-install.sh $host $password stop
 docker run -it exemple_using_opencv_tesseract_1_0 /opt/axis/acapsdk/sysroots/x86_64-pokysdk-linux/usr/bin/eap-install.sh $host $password start
