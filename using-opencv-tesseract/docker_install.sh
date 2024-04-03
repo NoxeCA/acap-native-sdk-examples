e300e5dd5580ec97e16be3ebfa88bbdce58f03c0
Mathieu Benoit <mathben@technolibre.ca>
2024-04-03 13:00:59 -0400

    [ADD] using-opencv-tesseract libm.so.6

--- using-opencv-tesseract/docker_install.sh
+++ using-opencv-tesseract/docker_install.sh
@@ -14,7 +14,7 @@ docker run -it exemple_using_opencv_tesseract_1_0 /opt/axis/acapsdk/sysroots/x86
 
 # Bug replace lib
 # libstdc++.so.6.0.30
-scp lib/libstdc++.so.6 root@${host}:/mnt/flash/usr/local/packages/opencv_tesseract_app/lib/libstdc++.so.6
+scp lib/*.so* root@${host}:/mnt/flash/usr/local/packages/opencv_tesseract_app/lib/
 
 docker run -it exemple_using_opencv_tesseract_1_0 /opt/axis/acapsdk/sysroots/x86_64-pokysdk-linux/usr/bin/eap-install.sh $host $password stop
 docker run -it exemple_using_opencv_tesseract_1_0 /opt/axis/acapsdk/sysroots/x86_64-pokysdk-linux/usr/bin/eap-install.sh $host $password start
